e3c63e873af4f90339cccba6a1c33497baba1307
felipeunesp <felipeunesp@gmail.com>
2016-04-29 14:52:52 -0300

    fafa

--- 2048.js
+++ 2048.js
@@ -7,6 +7,9 @@ var CasaTabuleiro = function () {
             this.valor = 0;
             this.fechada = 0;
         };
+        this.estaVazia = function () {
+            return this.valor === 0;
+        };
         this.setValor = function (valor) {
             this.valor = valor || 0;
         };
@@ -18,65 +21,110 @@ var Tabuleiro = function () {
     var i = 0, j = 0;
     
     this.tamanho = 4;
+    this.iniciado = 0;
     this.casas = [];
-    this.iterar = function (callback) {
-        console.log(i);
-        console.log(j);
+    this.iterar = function (obj, callback) {
+        var i=0, j=0;
         
-        // for (i; i < this.tamanho; i++) {
-        //    for (j; j < this.tamanho; j++) {
-        if (i == this.tamanho && j == this.tamanho) {
-            i = 0;
-            j = 0;
-            return false;
-        } else if (i == this.tamanho && j < this.tamanho) {
-            j++;
-        } else if (i < this.tamanho && j < this.tamanho) {
-            i++;
+        for (i=0; i<this.tamanho; i++) {
+            for (j=0; j<this.tamanho; j++) {        
+                if (callback.call(obj[i][j]) === false) {
+                    break;
+                }
+            }
         }
-        
-        
-        
-        this.casas[i] = [];
-        this.casas[i][j];
-        
-        //}
-        // };
-        
-        
+
+        return obj;
     };
     this.gameOver = function () {
-            
+        return !this.iterar(this.casas, estaVazia);
     };
     this.iniciar = function (tamanho) {
-        this.gameOver = 0;
-        this.tamanho = tamanho || 4; 
-    };
-    this.casaRandom = function () {
-        var x = Math.ceil(Math.random() * this.tamanho), y = Math.ceil(Math.random() * this.tamanho);
+        var i=0; j=0;
         
-        if ( !this.gameOver ) {
-       //    while () 
+        this.tamanho = tamanho || 4;
+        
+        if (this.iniciado === 0) {
+            for (i=0; i<this.tamanho; i++) {
+                this.casas[i]=[];
+                for (j=0; j<this.tamanho; j++) {    
+                    this.casas[i].push(new CasaTabuleiro());
+                }
+            }
+            this.iniciado = 1;
         }
         
-        return this.casas[x][y];
+        return true;
+    };
+    this.casaRandom = function () {
+        var x = Math.ceil(Math.random() * this.tamanho) - 1, y = Math.ceil(Math.random() * this.tamanho) - 1;
+        
+        if (this.iniciado === 0) {
+            this.iniciar(this.tamanho);
+        }
+        if ( this.casas[x][y].estaVazia() ) {
+            return this.casas[x][y];
+        } else {
+            this.casaRandom();
+        }
     };
     this.iniciarCasa = function (valor) {
         var casa = this.casaRandom(), valorCasa = valor || 2;
         
-        
-        
         casa.setValor(valorCasa);
     };
+    this.moveLeft = function () {
+        var i=0, j=0, limite=0;
+        
+        this.iterar(this.casas, function () { this.fechada = 0; });
+        
+        for (i=0;i<this.tamanho;i++) {
+            for (j=0;j<this.tamanho;j++) {
+                var p=j;
+                if (p === limite) {
+                    continue;
+                } else {
+                    while (p-1 >= limite) {
+                        
+                        if ((this.casas[i][p-1].estaVazia() || this.casas[i][p-1].valor === this.casas[i][p].valor) && !this.casas[i][p-1].fechada){
+                            this.casas[i][p-1].valor += this.casas[i][p].valor;
+                            this.casas[i][p].valor = 0;
+                            this.casas[i][p-1].fechada = this.casas[i][p-1].estaVazia()?0:1;
+                        }
+                        
+                        p = p-1;
+                    }
+                }
+            }
+        }
+    }
+    this.pintar = function (){
+        var d, i, j;
+        for ( i = 0; i<this.tamanho; i++ ) {
+            for ( j = 0; j<this.tamanho; j++ ) {
+                d = document.getElementById('td'+ i + j);
+                d.innerHTML = this.casas[i][j].valor;
+            }
+        }
+    };
 };
-  
+
 var tabuleiro = new Tabuleiro();
 tabuleiro.iniciar(4);
+tabuleiro.iniciarCasa(2);
+tabuleiro.iniciarCasa(2);
+
+var cont = 0;
+var x = setInterval( function(){
     
-var x;
-while ( tabuleiro.iterar() ) {
-    // 
+    tabuleiro.pintar();
+    tabuleiro.iniciarCasa(2);
+    tabuleiro.moveLeft();
     
-    console.log( x );
-}
-console.log(tabuleiro);
+    if (cont === 10) {
+        clearInterval(x)
+    };
+    cont++;
+    
+    
+}, 1000);
